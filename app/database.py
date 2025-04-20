6b047628782190defeb39f42a49787bba54ebf27
Sabdhx <syedaliabdullahshah5@gmail.com>
2025-04-20 20:23:54 +0500

    changes

---
+++ app/database.py
@@ -0,0 +1,11 @@
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+ 
+sqlite_file_name = "postgresql://postgres:123asd$@localhost/postgres"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
