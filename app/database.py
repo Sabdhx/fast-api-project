2e93af76a8c3697403ca4df69ed422f50be297c3
Sabdhx <syedaliabdullahshah5@gmail.com>
2025-04-20 20:24:41 +0500

    changes

--- app/database.py
+++ app/database.py
@@ -7,5 +7,5 @@ from sqlmodel import Field, Session, SQLModel, create_engine, select
 sqlite_file_name = "postgresql://postgres:123asd$@localhost/postgres"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+
 
